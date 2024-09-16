34e267d3c9b94b995f745d71009028b9d333316b
ciprian <ciprianvlad888@gmail.com>
2024-09-16 05:51:45 +0100

    created tables and connected db

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+Use App\Http\Controllers\StockController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/stock', [StockController::class,'store'])->name('');
+    Route::post('/stock', [StockController::class,''])->name('');
 });
 
 require __DIR__.'/auth.php';
